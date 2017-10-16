7755d765e9092cb7dcd5574fb90fc58f840644d7
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-16 00:16:20 -0400

    added conditonal logic to user input argument--which slects a spefic API to use

--- liri.js
+++ liri.js
@@ -8,3 +8,17 @@ var twitterKeys = require("./keys.js");
 
 // takes users command which should tell app which API to use
 var userSelectsAPI = process.argv[2];
+
+
+//====conditional statements to select which API to use=====
+if (userSelectsAPI === "my-tweets") {
+    console.log("tweet test worked");
+} else if (userSelectsAPI === "spotify-this-song") {
+    console.log("spotify api test worked");
+} else if (userSelectsAPI === "movie-this") {
+    console.log("movie test worked");
+} else if (userSelectsAPI === "do-what-it-says") {
+    console.log("do what it says worked");
+} else {
+    console.log("You've entered an incorrect command. Please enter a correct command to proceed.");
+}
