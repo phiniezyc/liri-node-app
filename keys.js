303f931284a5c8af9871f9e29666de7ab56565ae
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-17 00:50:36 -0400

    added getTweets Function but currently receiving error

--- keys.js
+++ keys.js
@@ -1,8 +1,10 @@
+console.log('this is loaded');
+
 var twitterKeys = {
     consumer_key: 'x2cbtGH7bOJToLkS7XLbAxgrL',
     consumer_secret: 'SKo41wQLF5ZaDcvw6g1SNjgTPVlgwD1taetGvBvgUgQ2BC6p01',
     access_token_key: '919736703547518978-bH0yV6IaNbt35IVBhWqy4yXKx0i17IR',
-    access_token_secret: '4KQdq1OhpbBnbIgmR5WzTtnayrwiF6tuV0vEXnslMDHMp',
+    access_token_secret: '4KQdq1OhpbBnbIgmR5WzTtnayrwiF6tuV0vEXnslMDHMp'
   };
 
   module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,24 +1,50 @@
 //============NPM Packages==============================
 var request = require("request");
 var nodeSpotifyApi = require("node-spotify-api");
-var twitter = require("twitter");
+var Twitter = require("twitter");
 //imported twitter keys module
-var twitterKeys = require("./keys.js");
+var keys = require("./keys.js");
+
+
+var client = new Twitter(keys.twitterKeys);
+
 //==========================================================
 
 // takes users command which should tell app which API to use
 var userSelectsAPI = process.argv[2];
 
 
+
+//==========Global Functions==========================
+
+function getTweets() {
+    var params = { screen_name: 'UGA_FB_Thoughts', count: 2 };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("HERE ARE YOUR TWEETS:");
+            console.log(tweets);
+        } else {
+           console.log("Sorry, there was an error");
+        }
+     });
+}
+
+
 //====conditional statements to select which API to use=====
 if (userSelectsAPI === "my-tweets") {
-    console.log("tweet test worked");
+    getTweets();
+
 } else if (userSelectsAPI === "spotify-this-song") {
     console.log("spotify api test worked");
+
 } else if (userSelectsAPI === "movie-this") {
     console.log("movie test worked");
+
 } else if (userSelectsAPI === "do-what-it-says") {
     console.log("do what it says worked");
+
 } else {
     console.log("You've entered an incorrect command. Please enter a correct command to proceed.");
 }
+
