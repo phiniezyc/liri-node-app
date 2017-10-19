0e08cad0c60482040d34a281d22bb292be23ce89
Chance Phiniezy <phiniezyc@gmail.com>
2017-10-19 15:58:51 -0400

    pulls in all the desired info from OMDB--working correctly EXCEPT same issue w/ other API calls, does not go to my else condition if command is left blank.  Need to fix overall. Must be simple mistake w/ my logic.

--- liri.js
+++ liri.js
@@ -88,10 +88,19 @@ function getMovieInfo() {
             
               // If the request is successful
               if (!error && response.statusCode === 200) {
-            
-                // Parse the body of the site and recover just the imdbRating
-                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+                console.log("Title: " + JSON.parse(body).Title);
                 console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                
+                //uncomment JSON.parse(body) below to get other information from the API
+                //console.log(JSON.parse(body));
+              } else {
+                  console.log(error);
               }
             });
     } else {
@@ -99,7 +108,6 @@ function getMovieInfo() {
         console.log("you searched for nothing!");
     }
 }
-
 
 
 //====conditional statements to select which API to use=====
@@ -110,7 +118,6 @@ if (userSelectsAPI === "my-tweets") {
     getSpotifySongInfo();
 
 } else if (userSelectsAPI === "movie-this") {
-    //console.log("movie test worked");
     getMovieInfo();
 
 } else if (userSelectsAPI === "do-what-it-says") {
